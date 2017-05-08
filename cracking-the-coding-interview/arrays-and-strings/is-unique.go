199faaf28e75812e1d50c45aa393353a7c6d57d3
maciaszczykm <maciaszczykm@icloud.com>
2017-05-08 19:58:32 +0200

    Add new kata

---
+++ cracking-the-coding-interview/arrays-and-strings/is-unique.go
@@ -0,0 +1,40 @@
+// Implement and algorithm to determine if a string has all unique characters. What if you cannot use additional data
+// structures?
+
+package main
+
+import "fmt"
+
+const (
+	// Considering ASCII chars only.
+	maxNumberOfChars = 128
+	uniqueString     = "abc"
+	invalidString    = "test"
+	emptyString      = ""
+)
+
+func main() {
+	fmt.Println(isUnique(uniqueString))
+	fmt.Println(isUnique(invalidString))
+	fmt.Println(isUnique(emptyString))
+}
+
+func isUnique(str string) bool {
+	switch {
+	case len(str) > maxNumberOfChars:
+		return false
+	case len(str) < 2:
+		return true
+	default:
+		checkboard := make([]bool, maxNumberOfChars)
+		for _, char := range str {
+			if checkboard[char] {
+				// If it is true, then we see the same char for the second time.
+				return false
+			} else {
+				checkboard[char] = true
+			}
+		}
+		return true
+	}
+}
